e20e9b081a287a41bf0bd444c38f4f093aba443b
Radovan Obradovic <Radovan.Obradovic@gmail.com>
2017-10-22 16:08:53 +0200

    GValOperationsTest updated.

--- src/GValOperationsTest.cpp
+++ src/GValOperationsTest.cpp
@@ -19,6 +19,42 @@ SUTF_TEST(testUNeg)
 	Sutf::test(r.asInt() == -4);
 }
 
+SUTF_TEST(testMul)
+{
+	GVal r = mul(GVal(4), GVal(3));
+	Sutf::test(r.asInt() == 12);
+}
+
+SUTF_TEST(testDiv)
+{
+	GVal r = div(GVal(16), GVal(3));
+	Sutf::test(r.asInt() == 5);
+}
+
+SUTF_TEST(testMod)
+{
+	GVal r = mod(GVal(16), GVal(3));
+	Sutf::test(r.asInt() == 1);
+}
+
+SUTF_TEST(testEq)
+{
+	GVal r = eq(GVal(16), GVal(3));
+	Sutf::test(r.asBool() == false);
+}
+
+SUTF_TEST(testUNot)
+{
+	GVal r = unot(GVal(true));
+	Sutf::test(r.asBool() == false);
+}
+
+SUTF_TEST(testLt)
+{
+	GVal r = lt(GVal(16), GVal(3));
+	Sutf::test(r.asBool() == false);
+}
+
 int main(int argc, char *argv[])
 {
 	Sutf::runTests(argc, argv);
